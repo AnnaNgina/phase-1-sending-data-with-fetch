d3d5b84b6ea232152fd022cfe5cd48cb62fb917b
annastacia <annastaciangina96@gmail.com>
2022-06-27 22:37:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    let data = {
+        "name": userName,
+        "email": userEmail
+    }
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+    })
+        .then(res => res.json())
+        .then(data => {
+            document.body.innerHTML = `${data.id}`
+        })
+        .catch((error) => {
+            console.log(error.message)
+            document.body.innerHTML = error.message
+        })
+}
